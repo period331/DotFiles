450acffbb5461abc5c01282e6a31c096fb9e1c12
baocaixiong <baocaixiong@gmail.com>
2016-04-22 15:29:22 +0800

    update

--- nginx-php.sh
+++ nginx-php.sh
@@ -1,53 +1,54 @@
 #!/bin/sh
 
 NGINX="/usr/local/bin/nginx"
-PHP5329="$HOME/.phpbrew/php/php-5.3.29/sbin/php-fpm.dSYM"
-PHP5618="/usr/local/Cellar/php56/5.6.18/sbin/php-fpm"
-PHP70="$HOME/.phpbrew/php/php-7.0.5/sbin/php-fpm"
+PHP5329="$HOME/.phpbrew/php/php-5.3.29/sbin/php-fpm.dSYM -y $HOME/.phpbrew/php/php-5.3.29/etc/php-fpm.conf -D"
+PHP5534="$HOME/.phpbrew/php/php-5.5.34/sbin/php-fpm -y $HOME/.phpbrew/php/php-5.5.34/etc/php-fpm.conf -D"
+PHP5618="/usr/local/Cellar/php56/5.6.18/sbin/php-fpm -y /usr/local/etc/php/5.6/php-fpm.conf -D"
+PHP70="$HOME/.phpbrew/php/php-7.0.5/sbin/php-fpm -y $HOME/.phpbrew/php/php-7.0.5/etc/php-fpm.conf -D"
 
-PHP5329_PID_PATH="/usr/local/var/run/php5.3-fpm.pid"
-PHP5618_PID_PATH="/usr/local/var/run/php-fpm.pid"
-PHP70_PID_PATH="/usr/local/var/run/php7-fpm.pid"
+PID_PATH="/usr/local/var/run/"
 
-NGINX_PID_PATH="/usr/local/var/run/nginx.pid"
+start_app() {
+    command=$1
+    ver=$2
+    pid="$ver.pid"
+
+    if [ -f $pid ]; then
+        echo "> ×× $ver is running ..."
+    else
+        `sudo $command`
+    fi
+}
+
+stop_app() {
+    ver=$1
+    pid="$PID_PATH$ver.pid"
+    if [ -f $pid ]; then
+        sudo kill `cat $pid`
+    else
+        echo "> ×× $ver is not running."
+    fi
+}
 
 start () {
     echo "Start php-fpm ..."
-
-    if [ -f $PHP5329_PID_PATH ]; then
-        echo "> × PHP 5.3.29 is running ..."
-    else
-        sudo $PHP5329 -y $HOME/.phpbrew/php/php-5.3.29/etc/php-fpm.conf -D
-    fi
-    if [ -f $PHP5618_PID_PATH ]; then
-        echo "> × PHP 5.6.18 is running ..."
-    else
-        sudo $PHP5618 -y /usr/local/etc/php/5.6/php-fpm.conf -D
-    fi
-
-    if [ -f $PHP70_PID_PATH ]; then
-        echo "> × PHP 7.0.5 is running ..."
-    else
-        sudo $PHP70 -y $HOME/.phpbrew/php/php-7.0.5/etc/php-fpm.conf -D
-    fi
-    echo "Start nginx ..."
-    if [ -f $NGINX_PID_PATH ]; then
-        echo '> × Nginx is running ...'
-    else
-        sudo $NGINX
-    fi
+    start_app $PHP5329 "php5.3"
+    start_app $PHP5534 "php5.5"
+    start_app $PHP5618 "php5.6"
+    start_app $PHP70 "php7"
+    echo "Start Nginx ..."
+    start_app $NGINX "nginx"
     echo "Done!"
 }
 
 stop() {
-    echo "Stop nginx ..."
-    sudo kill `cat $NGINX_PID_PATH`
-
+    echo "Stop Nginx ..."
+    stop_app "nginx"
     echo "Stop php-fpm ..."
-    sudo kill `cat $PHP5329_PID_PATH`
-    sudo kill `cat $PHP5618_PID_PATH`
-    sudo kill `cat $PHP70_PID_PATH`
-
+    stop_app "php5.3-fpm"
+    stop_app "php5.5-fpm"
+    stop_app "php5.6-fpm"
+    stop_app "php7-fpm"
     echo "Done!"
 }
 
@@ -59,7 +60,5 @@ elif [ $1 = "restart" ]; then
     stop
     start
 else
-    echo "\nValid commands: "
-    echo "start | stop | restart"
+    echo "Valid commands: {start|stop|restart}"
 fi
-
